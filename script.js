2c9d4191f2eeb2a0ff770586bc826b815a40c376
Elinnoel Nunez <elinnoelnunez@outlook.com>
2021-12-19 15:05:31 -0600

    better grid bg color

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const createGrid = (size) => {
   for(let i = 0; i < size * size; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
-    pixel.style.backgroundColor = "white";
+    pixel.style.backgroundColor = "rgb(182, 178, 178)";
     pixel.style.border = `1px solid rgb(0, 0, 0, 0.05)`;
     gridContainer.appendChild(pixel);
   }
