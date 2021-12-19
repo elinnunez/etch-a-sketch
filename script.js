62609a8bab4cc30bd8f5efa537ebd29d73682ffb
Elinnoel Nunez <elinnoelnunez@outlook.com>
2021-12-19 14:59:30 -0600

    better ui and responsiveness

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const container = document.querySelector(".container");
+const buttons = document.querySelectorAll("button");
+const gridContainer = document.querySelector(".grid");
 
-const createGrid = (row, col) => {
-  container.style.setProperty("--grid-rows", row);
-  container.style.setProperty("--grid-cols", col);
-  for (i = 0; i < row * col; i++) {
-    let cell = document.createElement("div");
-    // cell.innerText = i + 1;
-    container.appendChild(cell).className = "grid-item";
+
+const createGrid = (size) => {
+  for(let i = 0; i < size * size; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.style.backgroundColor = "white";
+    pixel.style.border = `1px solid rgb(0, 0, 0, 0.05)`;
+    gridContainer.appendChild(pixel);
   }
-};
 
-createGrid(16, 16);
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, auto)`;
+}
+
+createGrid(32);
